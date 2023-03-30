1aeb6e91f6282d77c1ed294551c6caa416d51167
texasjkee <gt.gluck@gmail.com>
2023-03-30 20:56:14 +0300

    initial project

---
+++ VanillaNodeJS/http/data/db.js
@@ -0,0 +1,8 @@
+module.exports =
+ [
+  {id:1, title: 'Alabama'},
+  {id:2, title: 'Texas'},
+  {id:3, title: 'Idaho'},
+  {id:4, title: 'Arizona'},
+  {id:5, title: 'Colorado'},
+];
---
+++ VanillaNodeJS/http/server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+
+const data = require('./data/db.js');
+
+const PORT = 3333;
+
+//https:www.example.ua/id:1  -valid URL
+const app = http.createServer((req, res) => {
+  
+  const validIdNumber = req.url.split(':').filter(el => !!Number(el));
+  const idUrl = `/id:${validIdNumber}`;
+  const foundId = data.find(data => data.id == [validIdNumber[0]]);
+
+  const foundTitle = foundId ? foundId.title : 'Id not found';
+  const validTitle = JSON.stringify(foundTitle);
+
+  switch(req.url){
+    case '/': res.write('Hello dude :)')
+    break;
+    case idUrl: res.write(validTitle)
+    break;
+    default: res.write('Bad request')
+  }  
+
+  res.end();
+})
+
+app.listen(PORT, () => {
+  console.log(`Running server on ${PORT}`)
+});
