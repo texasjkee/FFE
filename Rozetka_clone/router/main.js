650140989ad6763055b92f6d9ae97d6c4b8e1b9f
texasjkee <gt.gluck@gmail.com>
2023-04-24 12:17:42 +0300

    add: jes template

--- Rozetka_clone/router/main.js
+++ Rozetka_clone/router/main.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
+const products = require('../dataBase/products');
+
 const route = express.Router();
 
-route.get('/user', (req, res) => {
-  res.render('index')
+route.get('/products/:id', (req, res) => {
+  //TO_DO: url/:id & url:id
+  const urlId = Number(req.params.id) 
+  const product = products.find(el => el.id === urlId)
+  if(product) {
+    res.render('index', {product})
+  } else {
+   res.end('Bad request') 
+  }
 })
 
 module.exports = route;
