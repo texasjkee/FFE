eee185c4cdae21524cd57d464f27684fffc59c44
texasjkee <gt.gluck@gmail.com>
2023-04-24 13:19:49 +0300

    add: async methods

---
+++ Asynchronous/async.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+(async () => {
+  console.log('queue #1')
+  const text = await fs.promises.readFile('./example.txt', 'utf-8', err => console.log(err))
+  const content = `${text} + \n Yo babe`;
+
+  console.log('queue #2')
+  await fs.promises.writeFile('./example.txt', content, err => console.log(err))
+})();
+
+console.log('End')
