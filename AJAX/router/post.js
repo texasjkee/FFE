d7bc597c8d0fa121c332084afe6fbce13c2613a8
texasjkee <gt.gluck@gmail.com>
2023-05-05 10:59:47 +0300

    fix: searchFilter function

--- AJAX/router/post.js
+++ AJAX/router/post.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const route = express.Router();
 
-const dataBase = [];
+const dataBase = [
+  {
+    id: 1683206371112,
+    title: 'Post#1',
+    body: 'qweqweqwe',
+    hashtag: 'biology'
+  },
+  {
+    id: 1683206378814,
+    title: 'Post#2',
+    body: 'qweqweqweasdasa',
+    hashtag: 'security'
+  },
+  {
+    id: 1683206386023,
+    title: 'Post#3',
+    body: 'dfsalkflkjsdalfkjsadfsadf',
+    hashtag: 'policy'
+  },
+  {
+    id: 1683206386231,
+    title: 'Post#4',
+    body: 'Qlaksdjlasjdfsalkflkjsdalfkjsadfsadf',
+    hashtag: 'policy'
+  }
+];
 
 route.post('/posts?', (req, res) => {
   const postValue = req.body.post;
 
+  console.log(dataBase)
   if(postValue?.title) {
     dataBase.push(req.body.post);
     //TO_DO: 2 similar titles;??? 
@@ -17,9 +43,14 @@ route.post('/posts?', (req, res) => {
 
 route.post('/filter?', (req, res) => {
   const filterValue = req.body.filter;
-  const foundPostByHashtag = dataBase.find(post => post.hashtag === filterValue);
+  const foundPostByHashtag = dataBase.filter(post => post.hashtag.includes(filterValue) === true);
 
-  res.json({message: foundPostByHashtag});
+  console.log(filterValue);
+
+  if(foundPostByHashtag.length > 0){
+    res.json({message: foundPostByHashtag});
+  }
+  res.json({message: filterValue});
 })
 
 module.exports = route;
