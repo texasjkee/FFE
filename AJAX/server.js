7b237be9505bbfde75c572f2e587405379140cdc
texasjkee <gt.gluck@gmail.com>
2023-05-16 18:19:29 +0300

    add: fileRoute

--- AJAX/server.js
+++ AJAX/server.js
@@ -3,10 +3,11 @@ const server = express();
 
 const postRoute = require('./router/postRoute');
 const formRoute = require('./router/formRoute');
+const fileRoute = require('./router/fileRoute');
 
 const PORT = 3333;
 
-server.use(express.json())
+server.use(express.json());
 server.use(express.static('public'));
 server.use(express.urlencoded({ extended: true }));
 // TO_DO: Why is this needed?
@@ -22,10 +23,14 @@ server.get('/form',(req, res) => {
   res.render('form');
 })
 
+server.get('/file',(req, res) => {
+  res.render('file');
+})
+
 server.use(postRoute);
 server.use(formRoute);
+server.use(fileRoute);
 
 server.listen(PORT, () => {
   console.log(`Listening on ${PORT} port`)
 })
-
