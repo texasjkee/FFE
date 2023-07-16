29156518e8f6a45cdf4fc472976c1313c48ece92
Michael <gt.gluck@gmail.com>
2023-07-16 21:11:14 +0300

    add: index.js

--- mySession/server.js
+++ mySession/server.js
@@ -1,55 +1,53 @@
-const express = require('express');
-const server = express();
-const mongoose = require('mongoose');
-const session = require('express-session');
-const redis = require('redis');
-const connectRedis = require('connect-redis')
-const RedisStore = connectRedis(session);
-const redisClient = redis.createClient({url: 'redis://127.0.0.1:6379'});
-// const redis = require('ioredis');
-// const redisClient = redis.createClient({host:'127.0.0.1', port: 6379});
+// const express = require('express');
+// const session = require('express-session');
+// const redis = require('redis');
+// const RedisStore = require('connect-redis')(session);
 
-const PORT = 4000;
+// const app = express();
+var express = require('express');
+var redis   = require("redis");
+var session = require('express-session');
+var redisStore = require('connect-redis')(session);
+var client  = redis.createClient();
+var app = express();
 
-const loginRouter = require('./routers/authRouter');
-
-redisClient.on('error', err => console.log('Redis Client Errorjkee', err));
-redisClient.on('connect',() => {
-    console.log('connected to redis successfully!');
-})
-
-server.use(session({
-  store: new RedisStore({client: redisClient}),
-  secret: 'texasjkee',
-  saveUninitialized: false,
-  cookie: {
-    secure: false, // if true: only transmit cookie over https
-    httpOnly: true, // if true: it prevents client side JS from reading the cookie
-    maxAge: 1000 * 60 * 30 // session max age in miliseconds
-  }
+app.use(session({
+    secret: 'ssshhhhh',
+    store: new redisStore({ host: 'localhost', port: 6379, client: client}),
+    saveUninitialized: false,
+    resave: false
 }));
 
-server.use(express.json());
-server.use(express.static('public'));
+// const redisClient = redis.createClient()
+// redisClient.connect().catch(console.error)
 
-// server.use('/auth', loginRouter);
+// const redisStore = new RedisStore({
+//   client: redisClient,
+//   prefix: "myapp:",
+// })
 
-server.post('/login', (req, res) => {
+// app.use(session({
+//     store:  redisStore,
+//     secret: 'mySecret',
+//     saveUninitialized: false,
+//     resave: false, 
+//     cookie: {
+//         secure: false, // if true: only transmit cookie over https
+//         httpOnly: true, // if true: prevents client side JS from reading the cookie
+//         maxAge: 1000 * 60 * 30, // session max age in milliseconds
+//         sameSite: 'lax' // make sure sameSite is not none
+//     }
+// }));
+
+app.post('/login', (req, res) => {
     const {email, password} = req;
-
-    // check if the credentials are correct
-    // ...
-
-    // assume that credentials are correct
     req.session.clientId = 'abc123';
     req.session.myNum = 5;
 
     res.json('you are now logged in');
 });
 
-// 4. plug in another middleware that will check if the user is authenticated or not
-// all requests that are plugged in after this middleware will only be accessible if the user is logged in
-server.use((req, res, next) => {
+app.use((req, res, next) => {
     if (!req.session || !req.session.clientId) {
         const err = new Error('You shall not pass');
         err.statusCode = 401;
@@ -58,23 +56,8 @@ server.use((req, res, next) => {
     next();
 });
 
-// 5. plug in all routes that the user can only access if logged in
-server.get('/profile', (req, res) => {
+app.get('/profile', (req, res) => {
     res.json(req.session);
 });
 
-server.use((err, req, res, next) => {
-    console.log(err.stack);
-    console.log(err.name);
-    console.log(err.code);
-    
-    res.status(500).json({
-      message: 'Something went really wrong'
-    });
-});
-
-server.listen(PORT, () => console.log(`Server running on ${PORT} `));
-
-mongoose.connect('mongodb://127.0.0.1:27017/FFE')
-    .then(()=> console.log('Connected to mongoDB'))
-    .catch((err) => console.log(err));
+app.listen(8080, () => console.log('server is running on port 8080'));
