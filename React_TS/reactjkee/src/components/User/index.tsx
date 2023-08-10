982706dfa10c8119c743a4ceca0adb8adc8ff383
Michael <gt.gluck@gmail.com>
2023-08-10 20:42:21 +0300

    add: User components

---
+++ React_TS/reactjkee/src/components/User/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import './User.css'
+
+type AuthUser = {
+  name: string,
+  email: string,
+  age: number,
+  sex: "male" | "female"
+  adress?: string
+};
+
+const User = () => {
+  const [user, setUser] = useState <AuthUser | null>(null);
+  const handleLogin = () => {
+    setUser({
+      name: "John",
+      email: "example@gmail.com",
+      age: 47,
+      sex: "male",
+    });
+  };
+  const handleLoggout = () => { setUser(null)};
+  
+  return(
+    <div className="none">
+      <button onClick={handleLogin}>Login</button>      
+      <button onClick={handleLoggout}>Logout</button>      
+      <div>User is {user?.name} </div>
+      <div>User is {user?.email} </div>
+    </div>
+  );
+};
+
+export default User;
